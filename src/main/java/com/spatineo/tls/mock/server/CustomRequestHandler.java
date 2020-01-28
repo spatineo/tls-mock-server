8c1ac8f461f8b443054b0bf0d451babdc930b511
patrick-alaspaa <patrick.alaspaa@spatineo.com>
2020-01-28 11:01:11 +0200

    Modify custom request handler in order to return different content types

--- src/main/java/com/spatineo/tls/mock/server/CustomRequestHandler.java
+++ src/main/java/com/spatineo/tls/mock/server/CustomRequestHandler.java
@@ -10,23 +10,42 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 public class CustomRequestHandler extends AbstractHandler {
-    public CustomRequestHandler() { }
+    private String contentType;
+    public CustomRequestHandler() {
+        contentType = Const.CONTENT_TYPE_HTML;
+    }
+
+    public CustomRequestHandler(String contentType) {
+        this.contentType = contentType;
+    }
 
     @Override
     public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
             throws IOException, ServletException {
-        System.out.println("Request: ");
-        System.out.println(request.toString());
-        response.setContentType(Const.RESPONSE_CONTENT_TYPE);
+        response.setContentType(contentType);
         response.setStatus(HttpServletResponse.SC_OK);
 
         PrintWriter out = response.getWriter();
+        out.println(getResponseMessage(Integer.toString(request.getServerPort()), request.getContextPath()));
 
         System.out.println("Response: ");
         System.out.println(response.toString());
-        out.println("<h1>" + Const.SERVER_GREETING + request.getServerPort() + " " + target + "</h1>");
 
         baseRequest.setHandled(true);
     }
 
+    private String getResponseMessage(String port, String contextpath) {
+        String message;
+        if(contentType.equals(Const.CONTENT_TYPE_HTML)) {
+            message = "<h1>" + Const.SERVER_GREETING + " " + port + " " + contextpath + "</h1>";
+        } else if(contentType.equals(Const.CONTENT_TYPE_JSON)) {
+            message = "{\"message\":\"" + Const.SERVER_GREETING + "\", \"port\":\"" + port + "\",\"path\":\"" + contextpath + "\"}";
+        } else if(contentType.equals(Const.CONTENT_TYPE_XML)) {
+            message = "<?xml version=\"1.0\" encoding=\"UTF-8\"?><response><message>" + Const.SERVER_GREETING + "</message><port>" + port + "</port><path>" + contextpath + "</path></response>";
+        }
+         else {
+            message = "Hello World!";
+        }
+        return message;
+    }
 }
